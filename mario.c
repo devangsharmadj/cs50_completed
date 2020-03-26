d1c3b25dcd4414f02106c7162c011f7ee2319a5f
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-03-26 19:35:23 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -6,14 +6,14 @@ int main(void)
     int n;
     do
     {
-        n=get_int("Height:");
+        n = get_int("Height: ");
     }
-    while(n<0 || n>9 );
-    for(int i=0; i<n ; i++)
+    while (n < 1 || n  > 8);
+    for (int i = 0; i < n ; i++)
     {
-        for( int j=0; j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            if(n-1-i>j)
+            if (n - 1 - i > j)
             {
                 printf(" ");
             }
