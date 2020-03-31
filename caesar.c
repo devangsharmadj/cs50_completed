6587234559399da9ae24ef0d479173754a50d465
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-03-31 19:47:42 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,28 +1,33 @@
 // @ author Devang Sharma
 // Encryption code
-
 #include<cs50.h>
 #include<stdio.h>
 #include<string.h>
-
-
-
+#include<stdlib.h>
 int main(int argc, string argv[])
-
 {
-
     string s;
     int key;
     if(argc == 2)
     {
         // validate that argument is integer
-        // save it into variable key
-        key = (int)argv[1];
+        // save it into variable
+        //key = get_int(argv[1]);
+       // printf ("< %s >< %i >",argv[0],atoi(argv[1]));
+        key = atoi(argv[1]);
     }
     else
     {
         // print error message
         return 1;
+    }
+    if (key>=26 )
+    {
+        key = (key)%26;
+    }
+    else
+    {
+        key = (key + 0);
     }
     //int key = get_int("key: ");
     int n;
@@ -31,31 +36,23 @@ int main(int argc, string argv[])
     n = strlen(s);
     char y[n];
     printf("ciphertext: ");
+    
 
     for (int i = 0; i < n; i++)
     {
-        if ((int)s[i] > 47 && (int)s[i] < 65)
+      int t = (int)s[i] ; 
+        if (64 <  t && t < 91 )
         {
-            // Code for numbers
-            y[i] = s[i];
+            // Code for letters
+            y[i] = (char)((int)s[i] + key);
         }
-        else  if ((int)s[i] + key > 122)
+        else if (96 < t && t < 123)
         {
-            // Code for when the integer value of lowercase number goes over z
-            y[i] = (char)((int)s[i] + key - 122 + 96) ;
+             y[i] = (char)((int)s[i] + key);
         }
         else
         {
-            if ((int)s[i] + key > 91 && (int)s[i] + key < 96)
-            {
-                // Code for if the integer value goes over capital Z
-                y[i] = (char)((int)s[i] + key - 91 + 65);
-            }
-            else
-            {
-                // Code for normal letters
-                y[i] = (char)((int)s[i] + key);
-            }
+             y[i] = s[i];
         }
         
     }
