02b96944f30b2d9fa04c6adf0ff02ad7f2148487
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-03-31 18:44:48 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -7,18 +7,30 @@
 
 
 
-int main(void)
+int main(int argc, string argv[])
 
 {
 
     string s;
-    int key = get_int("key: ");
+    int key;
+    if(argc == 2)
+    {
+        // validate that argument is integer
+        // save it into variable key
+        key = (int)argv[1];
+    }
+    else
+    {
+        // print error message
+        return 1;
+    }
+    //int key = get_int("key: ");
     int n;
 
     s = get_string("plaintext: ");
     n = strlen(s);
     char y[n];
-    printf("Ciphertext: ");
+    printf("ciphertext: ");
 
     for (int i = 0; i < n; i++)
     {
@@ -41,7 +53,7 @@ int main(void)
             }
             else
             {
-                // Code for normal numbers
+                // Code for normal letters
                 y[i] = (char)((int)s[i] + key);
             }
         }
