1715aa3fd9c5c0812c472d5cbf630c31bcb4802a
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-02 21:18:02 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -1,3 +1,7 @@
+// @ author Devang Sharma
+// Finding the U.S.A. Grade level for any text
+// Using Coleman - Liau index
+
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,44 +20,24 @@ int main(void)
     {
         int t = (int)s[i];
 
-         if(((t > 96 && t < 123) || (t > 64 && t < 91)))
-         {
-             l = l + 1;
-         }
-         else
-         {
-            l = l + 0;
-         }
-
-
-
-    }
-    for (int i = 0 ; i < strlen(s) ; i ++)
-    {
-        int t = (int)s[i];
-
-         if(t == 46 || t == 63 || t == 33)
-         {
-             sent = sent + 1;
-         }
-         else
-         {
-            sent = sent + 0;
-         }
-    }
-    for (int i = 0 ; i < strlen(s) ; i ++)
-    {
-        int t = (int)s[i];
-        if (t == 32)
+        if (((t > 96 && t < 123) || (t > 64 && t < 91)))
         {
+            // Counting the number of Letters in the text
+            l = l + 1;
+        }
+        else if( t == 46 || t == 63 || t == 33)
+        {
+            // Counting the numbers of sentences
+            sent = sent + 1;
+        }
+        else if(t == 32)
+        {
+            // Counting the number of words
             w = w + 1;
         }
-        else
-        {
-            w = w + 0;
-        }
+
     }
-    if(grade(w, sent, l) < 1)
+    if (grade(w, sent, l) < 1)
     {
         printf("Before Grade 1\n");
     }
@@ -66,16 +50,14 @@ int main(void)
         printf("Grade %i\n", grade(w, sent, l));
     }
 }
-
+// Function for the calculations
 int grade(int w, int sent, int l)
 {
+    // Applying the Coleman-Liau index
 
-    double L = 100 * l / w;
-    double S = 100 * sent / w;
+    float L = 100 * (float)l / w;
+    float S = 100 * (float)sent / w;
     float index = .0588 * L - .296 * S - 15.8;
     int result = round(index);
-    
-    
     return result;
-
 }
