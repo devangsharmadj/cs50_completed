e45f6f301fc43f2177773be4ed880273190b7143
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-13 20:46:14 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -77,7 +77,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i ++)
     {
-        for (int j = 0; j < width; j ++)
+        for (int j = 0; j < width / 2; j ++)
         {
             int r = image[i][j].rgbtRed ;
             int g = image[i][j].rgbtGreen ;
