5eafc73f4559541a77603f904e6dea154f288878
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-15 19:12:55 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -223,7 +223,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j ++)
         {
             image[i][j].rgbtRed = temp[i][j].rgbtRed;
-            image[i][j].rgbtBlue = temp[i][j].rgbtRed;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
             image[i][j].rgbtGreen = image[i][j].rgbtGreen;
         }
     }
