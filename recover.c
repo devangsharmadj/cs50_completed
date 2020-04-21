757f9c4b4e35087ca75f866999f90cc894e9ee27
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-21 21:44:08 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -11,26 +11,30 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover file\n");
         return 1;
     }
-    FILE *inputFilePtr = fopen(argv[1], "rb");
+    FILE* inputFilePtr = fopen(argv[1], "rb");
     if (inputFilePtr == NULL)
     {
         return 1;
     }
     int number = -1;
     char fileName[8];
-    FILE* image = NULL;
+    FILE *image = NULL;
     int size = 1;
     do
     {
         BYTE buffer[512];
         size = fread(buffer, sizeof(buffer), 1, inputFilePtr);
+        if (size == 0)
+        {
+            break;
+        }
         //printf("_%i_<%i>", number, size);
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             number = number + 1;
             if (image != NULL)
             {
-              fclose(image);
+                fclose(image);
             }
             sprintf(fileName, "%03i.jpg", number);
             image = fopen(fileName, "w");
@@ -46,8 +50,8 @@ int main(int argc, char *argv[])
         fclose(image);
     }
     return 0;
-     //if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
-   //             {
+    //if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
+    //             {
     // Check if file is a Jpeg
 
 
