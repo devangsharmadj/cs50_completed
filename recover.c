c5358333c49cbaed335f0b72679a4b455933579b
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-21 19:13:39 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cs50.h>
+#include <stdint.h>
 
+typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -9,36 +11,51 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover file\n");
         return 1;
     }
-    FILE *inputFilePtr = fopen(argv[1], "r");
+    FILE *inputFilePtr = fopen(argv[1], "rb");
     if (inputFilePtr == NULL)
     {
         return 1;
     }
 
-    int number=0;
-    string fileName = "File";
+    int number = -1;
+    char fileName[8];
     FILE* outputFile;
-    int size = 0;
+    int size = 1;
     do
     {
-        int buffer[512];
+        BYTE buffer[512];
         size = fread(buffer, sizeof(buffer), 1, inputFilePtr);
         for(int i = 0; i < 512; i ++)
         {
             if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
             {
                 number = number + 1;
-                do
-                {
+                printf("_%i_\n", number);
+                //do
+                //{
                     sprintf(fileName, "%03i.jpg", number);
-                    FILE *fp = fopen(fileName, "w");
-            
-                }
-                while (buffer[i] != 0xff && buffer[i + 1] != 0xd8 && buffer[i + 2] != 0xff && (buffer[i + 3] & 0xf0) != 0xe0);
+                    FILE *fp = fopen(fileName, "wb");
+                    if (fp == NULL)
+                    {
+                        printf("Error. Could not open file\n");
+                        exit (1);
+                    }
+                    BYTE buffer1[1];
+                    for (int j = i + 3; j < 512; j ++)
+                    {
+                        fwrite(buffer,sizeof(buffer1), 1, fp);
+                        if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
+                        {
+                            fclose(fp);
+                            break;
+                        }
+                    }
+               // }
+                //while (buffer[i] != 0xff && buffer[i + 1] != 0xd8 && buffer[i + 2] != 0xff && (buffer[i + 3] & 0xf0) != 0xe0);
             }
         }
-    } 
-    while (size == 512);
+    }
+    while (size == 1);
      //if (buffer[i] == 0xff && buffer[i + 1] == 0xd8 && buffer[i + 2] == 0xff && (buffer[i + 3] & 0xf0) == 0xe0)
    //             {
     // Check if file is a Jpeg
