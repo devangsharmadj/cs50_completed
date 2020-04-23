5d734c412b32b32c7df1fef11faaa99b61613ae1
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-04-23 00:04:37 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
+// @ author Devang Sharma
+// Program to say hello, name
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+    // Getting name of user
     string s = get_string("What is your name: ");
+    // Printing hello, name
     printf("hello, %s\n", s);
 }
