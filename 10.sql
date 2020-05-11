62f26c2be962ce6c2a8f7e0b68132aabc1b6a9d1
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-05-11 17:38:10 +0000

    automated commit by submit50

--- 10.sql
+++ 10.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(name) FROM people WHERE id IN 
 (SELECT person_id FROM directors WHERE movie_id IN
-(SELECT  movie_id FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating >= 9));
+(SELECT  movie_id FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating >= 9))
 
