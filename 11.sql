614e71919526fc7e17d7ff80a4b4c3c3113ce62f
devangsharmadj <devangsharmadj@users.noreply.github.com>
2020-05-11 17:12:39 +0000

    automated commit by submit50

---
+++ 11.sql
@@ -0,0 +1,6 @@
+SELECT title FROM(SELECT title, rating FROM ratings 
+JOIN movies ON movies.id = ratings.movie_id
+WHERE movie_id IN
+(SELECT movie_id FROM stars WHERE person_id IN
+(SELECT id FROM people WHERE name = 'Chadwick Boseman')))
+ORDER BY rating LIMIT 5;
